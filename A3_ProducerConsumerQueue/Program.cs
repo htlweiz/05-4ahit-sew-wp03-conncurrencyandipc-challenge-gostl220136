92cbf9cc604394c8d52b4f97ebeb52d023fb6385
gostl220136 <gostl220136@sr.htlweiz.at>
2025-11-10 09:08:34 +0100

    a3

--- A3_ProducerConsumerQueue/Program.cs
+++ A3_ProducerConsumerQueue/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,24 +10,31 @@ class Program
 {
     public static void Main(string[] args)
     {
+        var queue = new ConcurrentQueue<int>();
+
         Console.WriteLine("Übung 3: Producer-Consumer");
         Console.WriteLine("==========================================\n");
 
-       // TODO
-       
+        // TODO
+        List<Producer> producers = new List<Producer>();
+        for (int i = 0; i < 5; i++)
+        {
+            producers.Add(new Producer(i + 1, queue));
+        }
 
         Console.WriteLine("Producer und Consumer gestartet...\n");
 
         // Überwachung: Jede Sekunde Queue-Füllstand ausgeben und auf >50 prüfen
-        
+
         // TODO
+        foreach (var producer in producers)
+        {
+            if (queue.Count > 50)
+            {
+                producer.Stop();
+            }
+        }
 
 
-        // Alle Producer stoppen
-       
-
-        // Consumer stoppen
-       
-       
     }
 }
