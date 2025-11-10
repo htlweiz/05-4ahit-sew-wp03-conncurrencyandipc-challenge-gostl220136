5c16367999f09d368d655b0ff75973629c13c863
gostl220136 <gostl220136@sr.htlweiz.at>
2025-11-10 08:00:24 +0100

    a1 1.

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -10,17 +10,32 @@ class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
+
+        Thread threadA = new Thread(CountUpThreadA);
+        Thread threadB = new Thread(CountDownThreadB);
+        threadA.Start();
+        threadB.Start();
+        threadA.Join();
+        threadB.Join();
         
         
     }
     
     private static void CountUpThreadA()
     {
-        
+        for (int i = 0; i <= 100; i++)
+        {
+            Console.WriteLine($"Thread A: {i}");
+            Thread.Sleep(100);
+        }
     }
     
     private static void CountDownThreadB()
     {
-       
+        for (int i = 100; i >= 0; i--)
+        {
+            Console.WriteLine($"Thread B: {i}");
+            Thread.Sleep(100);
+        }
     }
 }
