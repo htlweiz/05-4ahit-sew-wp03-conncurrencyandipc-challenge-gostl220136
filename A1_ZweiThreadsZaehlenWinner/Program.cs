9acd651f2fd0b048e79dd34be4790c127506c539
gostl220136 <gostl220136@sr.htlweiz.at>
2025-11-15 11:05:12 +0100

    a1 small fixes

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -5,12 +5,13 @@ namespace A1_ZweiThreadsZaehlenWinner;
 
 class Program
 {
-    private static int threadAValue = 0;
-    private static int threadBValue = 0;
+    private static int threadAValue = 1;
+    private static int threadBValue = 100;
+    private static bool stopped = false;
 
     public static void Main(string[] args)
     {
-        Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
+        Console.WriteLine("Übung 1: Zwei Threads - Zählen & Winner");
 
         Thread threadA = new Thread(CountUpThreadA);
         Thread threadB = new Thread(CountDownThreadB);
@@ -18,17 +19,32 @@ class Program
         threadB.Start();
         threadA.Join();
         threadB.Join();
+
+        if (threadAValue < 50)
+        {
+            Console.WriteLine($"Thread B {threadBValue}");
+        }
+        else if (threadAValue == 50)
+        {
+            Console.WriteLine($"Tie {threadAValue} {threadBValue}");
+        }
+        else
+        {
+            Console.WriteLine($"Thread A {threadAValue}");
+        }
     }
     
     private static void CountUpThreadA()
     {
         for (int i = 1; i <= 100; i++)
         {
+            if (stopped) break;
             threadAValue = i;
 
             if (threadAValue == threadBValue)
             {
                 Console.WriteLine($"{threadAValue} {threadBValue}");
+                stopped = true;
             }
             Thread.Sleep(100);
         }
@@ -38,15 +54,15 @@ class Program
     {
         for (int i = 100; i >= 1; i--)
         {
+            if (stopped) break;
             threadBValue = i;
             
             if (threadAValue == threadBValue)
             {
                 Console.WriteLine($"{threadAValue} {threadBValue}");
+                stopped = true;
             }
             Thread.Sleep(100);
         }
     }
-
 }
-
